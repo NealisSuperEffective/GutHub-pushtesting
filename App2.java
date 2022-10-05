f895509ccb16c39066d0d8001ffbe59f8dc70c6a
NealAites <aitesneal@gmail.com>
2022-10-05 11:42:10 -0500

    Further testing with updated hello's

--- App2.java
+++ App2.java
@@ -1,5 +1,7 @@
 public class App2{
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Guten tag");
+        System.out.println("Hello World");
     }
 }
